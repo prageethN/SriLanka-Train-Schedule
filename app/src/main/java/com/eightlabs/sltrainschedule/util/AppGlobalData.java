dce04823b94fc16183f871d0c8f3f855a8594a49
removedporn <86824510+removedporn@users.noreply.github.com>
2022-03-08 13:19:43 +0800

    Delete app directory

--- app/src/main/java/com/eightlabs/sltrainschedule/util/AppGlobalData.java
+++
@@ -1,8 +0,0 @@
-package com.eightlabs.sltrainschedule.util;
-
-
-public class AppGlobalData {
-
-   public static String STR_SUPPORT_EMAIL = "eightmediasoft@outlook.com";
-   public static String STR_EMAIL_CHOOSER_HEADER = "Choose an Email client";
-}
